5ddf959aa8301d67ba69948ca708c299817c1504
Jayson DeMarchi <jayson.demarchi@gmail.com>
2018-10-11 17:58:29 -0400

    Reformat

--- src/AboutModal.js
+++ src/AboutModal.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 class AboutModal extends Component {
-    render() {
-        return (
-            <div>
-                <h1>AboutModal Component</h1>
-            </div>
-        );
-    }
+
+  render() {
+    return (
+      <div>
+        <h1>AboutModal Component</h1>
+      </div>
+    );
+  }
 }
 
 export default AboutModal;
