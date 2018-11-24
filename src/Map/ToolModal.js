5b66fd9b65c808d02f7dfeba105d67baa1578eea
henrylong <henry.cassetta.long@gmail.com>
2018-11-23 22:57:27 -0500

    Adds slider generation to ToolModal

--- src/Map/ToolModal.js
+++ src/Map/ToolModal.js
@@ -15,10 +15,24 @@ class ToolModal extends Component {
     stopAlgorithm();
   }
 
+  createSliders = (sliderCount) => {
+    let sliders = [];
+    for(let i = 0; i < sliderCount; i++){
+      sliders.push(
+        <div>
+          <label name={"weightlLabel" + i}>weight{i}</label>
+          <input type="range" id={"weightSlider" + i} name={"weightSlider" + i} min="0" max="10" defaultValue="5" step="1"></input>
+        </div>
+      );
+    }
+    return sliders;
+  }
+
   render() {
     return (
       <div className="Modal ToolModal">
         <h1>ToolModal Component</h1>
+        {this.createSliders(3)}
         <button onClick={() => this.onStart()}>Start</button>
         <button onClick={() => this.onPause()}>Pause</button>
         <button onClick={() => this.onStop()}>Stop</button>
