66db2f6683f4a21ff9ec8670efd0ef2bf186b4d5
henrylong <henry.cassetta.long@gmail.com>
2018-11-29 13:12:53 -0500

    cleans up DisplayModal.js

--- src/Map/DisplayModal.js
+++ src/Map/DisplayModal.js
@@ -6,29 +6,29 @@ class DisplayModal extends Component {
     this.props.clearOutput();
   }
 
-    render() {
-      if(this.props.zoomed){
-        return (
-            <div className="Modal DisplayModal">
-                <p id="outputTextArea" class="outputTextarea">
-                {
-                  this.props.terminalUpdates.map((text) => {
-                    return(
-                      <div>{text}<br></br><br></br></div>)
-                  })
-                }
-                </p>
-                
-                <button onClick={() => {this.clear()}}>Clear</button>
-            </div>
-        );
-      }
-      else{
-        return (
-          <div></div>
-        )
-      }
+  render() {
+    if(this.props.zoomed){
+      return (
+          <div className="Modal DisplayModal">
+              <p id="outputTextArea" class="outputTextarea">
+              {
+                this.props.terminalUpdates.map((text) => {
+                  return(
+                    <div>{text}<br></br><br></br></div>)
+                })
+              }
+              </p>
+              
+              <button onClick={() => {this.clear()}}>Clear</button>
+          </div>
+      );
     }
+    else{
+      return (
+        <div></div>
+      )
+    }
+  }
 }
 
 export default DisplayModal;
