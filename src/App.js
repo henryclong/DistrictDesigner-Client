959579e723c2273418a56584c44e53e5bb3fc795
henrylong <henry.cassetta.long@gmail.com>
2018-12-10 13:55:08 -0500

    adds styling to loginModal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import AboutModal from './AboutModal';
+import LoginModal from './LoginModal';
 import Header from './Header';
 import Map from './Map';
 import Modal from 'react-modal';
@@ -36,10 +37,20 @@ class App extends Component {
         />
         <Map />
         <Modal
+          className="Popup"
+          overlayClassName="PopupOverlay"
           isOpen={this.state.faq.isActive}
           onRequestClose={() => this.onToggle('faq')}
         >
           <AboutModal />
+        </Modal>
+        <Modal
+          className="Popup"
+          overlayClassName="PopupOverlay"
+          isOpen={this.state.login.isActive}
+          onRequestClose={() => this.onToggle('login')}
+        >
+          <LoginModal />
         </Modal>
       </div>
     );
