203efc6caa330494020ea61869bdfeb86bc187be
Jayson DeMarchi <jayson.demarchi@gmail.com>
2018-12-15 16:10:45 -0500

    Add User Account Connections
    User can now:
     - Create an account
     - Log in
     - Log out

--- src/helpers/district-designer.js
+++ src/helpers/district-designer.js
@@ -1,4 +1,61 @@
 import { URL, HTTP_STATE, HTTP_STATUS } from '../config/constants';
+import { generatePassword, validateAuth } from './utils';
+
+export const createAccount = (username, password) => {
+  const request = new XMLHttpRequest();
+  const body = JSON.stringify({
+    'username': username,
+    'password': generatePassword(password),
+  });
+  request.onreadystatechange = () => {
+    if (request.readyState === HTTP_STATE.DONE && request.status === HTTP_STATUS.OK) {
+      return JSON.parse(request.response);
+    }
+  }
+  request.open("POST", URL + "/CreateAccount", false);
+  try{
+    request.send(body);
+  } catch (e) {
+    return false;
+  }
+  return request.onreadystatechange();
+}
+
+export const authUser = (username, password) => {
+  const request = new XMLHttpRequest();
+  const body = JSON.stringify({
+    'username': username,
+    'password': '',
+  });
+  request.onreadystatechange = () => {
+    if (request.readyState === HTTP_STATE.DONE && request.status === HTTP_STATUS.OK) {
+      return JSON.parse(request.response);
+    }
+  }
+  request.open("POST", URL + "/LogIn", false);
+  try{
+    request.send(body);
+  } catch (e) {
+    return false;
+  }
+  return validateAuth(password, request.onreadystatechange());
+}
+
+export const logOut = () => {
+  const request = new XMLHttpRequest();
+  request.onreadystatechange = () => {
+    if (request.readyState === HTTP_STATE.DONE && request.status === HTTP_STATUS.OK) {
+      return JSON.parse(request.response);
+    }
+  }
+  request.open("GET", URL + "/LogOut", false);
+  try{
+    request.send();
+  } catch (e) {
+    return false;
+  }
+  return request.onreadystatechange();
+}
 
 export const getConstitution = (shortName) => {
   const request = new XMLHttpRequest();
