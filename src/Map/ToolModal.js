dbcbf67a6f21bc615c9b6ec428e9f763c9687ba1
henrylong <henry.cassetta.long@gmail.com>
2018-12-20 03:26:22 -0500

    sending weights works now

--- src/Map/ToolModal.js
+++ src/Map/ToolModal.js
@@ -20,11 +20,12 @@ class ToolModal extends Component {
       parameters: {},
       options: [],
       weightOptionsList: [],
+      weightsToSend: [],
     };
   }
 
   componentDidMount() {
-    this.props.weights.map((item) => (this.updateWeight(item.id, (this.props.sliderMax/2).toFixed(2))));
+    this.props.weights.map((item) => (this.initWeight(item.id, (this.props.sliderMax/2).toFixed(2))));
   }
 
   toggleConstitutionView = () => {
@@ -38,7 +39,7 @@ class ToolModal extends Component {
   }
 
   onStart = () => {
-    return this.props.onStart(this.state.weights, this.state.algorithm, this.state.parameters);
+    return this.props.onStart(this.state.weightsToSend, this.state.algorithm, this.state.parameters);
   }
   
   onStop = () => {
@@ -53,17 +54,45 @@ class ToolModal extends Component {
     this.setState({ parameters: parameters });
   }
 
+  initWeight = (sliderId, newWeight) => {
+    let newWeightsToSend = [];
+    this.props.weights.map(element => {
+      newWeightsToSend.push({
+        id: element.startId,
+        value: (newWeight / this.props.sliderMax).toFixed(2),
+      });
+    });
+    this.setState({ weightsToSend: newWeightsToSend });
+  }
+
   updateWeight = (sliderId, newWeight) => {
-    this.setState({ weights: this.state.weights.map(element => {
+    let newWeights = [];
+    let newWeightsToSend = [];
+    this.state.weights.map(element => {
       if (element.id === sliderId) {
-        return {
-          label: element.label,
+        newWeights.push({
           id: element.id,
+          startId: element.startId,
           value: (newWeight / this.props.sliderMax).toFixed(2),
-        }
+        });
+        newWeightsToSend.push({
+          id: element.startId,
+          value: (newWeight / this.props.sliderMax).toFixed(2),
+        });
+      } else {
+        newWeights.push({
+          id: element.id,
+          startId: element.startId,
+          value: element.value,
+        });
+        newWeightsToSend.push({
+          id: element.startId,
+          value: element.value,
+        });
       }
-      return element;
-    })});
+    });
+    this.setState({ weights: newWeights });
+    this.setState({ weightsToSend: newWeightsToSend });
   }
 
   zoomOut = () => {
